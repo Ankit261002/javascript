5e0c06d452f36c8a4e4ccd0d7055f7516a92456e
Ankit261002 <141931472+Ankit261002@users.noreply.github.com>
2023-11-22 10:26:02 +0000

    datatypes

---
+++ basic/third.js
@@ -0,0 +1,12 @@
+"use strict";// treat all js code as newer version
+// alert(3+3) we are using node js
+
+//number -> 2 to 2^53
+//bigint
+//string
+//boolean
+//null sdantalone value
+//undefine 
+//symbol
+//object
+console.log(typeof "ankit");

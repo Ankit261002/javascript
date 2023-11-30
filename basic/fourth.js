164dea7792415aae1cd66e84dd5d337aa0423d3e
Ankit261002 <141931472+Ankit261002@users.noreply.github.com>
2023-11-30 08:25:40 +0000

    fourthcode

---
+++ basic/fourth.js
@@ -0,0 +1,20 @@
+// let a=10;
+// let b=9;
+// console.log(a**b);
+
+// let a="12";
+// console.log(typeof a);
+
+// let b=Number(a);
+// console.log(typeof b);
+
+// ****************************comparision*************************
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+
+console.log("2"==2);
+
+console.log("2"===2);
+console.log(2===2);

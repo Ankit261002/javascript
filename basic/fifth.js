b080763b0377930c4e63af8290c41f72efd1982c
Ankit261002 <141931472+Ankit261002@users.noreply.github.com>
2023-11-30 09:01:52 +0000

    fifthjavascript

---
+++ basic/fifth.js
@@ -0,0 +1,23 @@
+// node basic/fifth.js
+// number and maths
+// const score=10000;
+// console.log(score.toString());
+// console.log(score.toString().length);
+// console.log(score.toPrecision(2));
+// console.log(score);
+
+// const score=123.66291;
+// console.log(score.toPrecision(3));
+
+// const a=103;
+// console.log(a.toFixed(3));
+
+
+// const a=12.63829;
+// console.log(Math.floor(a));
+
+// console.log(Math.round(a));
+
+ const min=1;
+ const max=6;
+ console.log(Math.floor(Math.round()*(max-min+1)+min));
